36d61043dbc36a7efe2aaa5e3c2992ebdff3309c
Andikarya17 <andikaryaputra17@gmail.com>
2024-05-21 14:50:05 +0700

    membuat akses public

--- Initialization.cpp
+++ Initialization.cpp
@@ -2,10 +2,18 @@
 #include <string>
 using namespace std;
 
+//Nama Repository = ParadigmaOOP1_4nimbelakang
+// minimal commit 15
 class mahasiswa {
 private:
     const int id;
     string nama;
     float nilai;
+
+    public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+    
+    }
+
 
 };
